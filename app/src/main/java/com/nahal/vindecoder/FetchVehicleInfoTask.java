5f0ed948bce6407aaf96cdbc882488398dcfae7a
Sukhjinder <nahal.snahal@gmail.com>
2017-02-04 16:20:48 -0500

    Added FetchVehicleInfoTask

---
+++ app/src/main/java/com/nahal/vindecoder/FetchVehicleInfoTask.java
@@ -0,0 +1,16 @@
+package com.nahal.vindecoder;
+
+import android.os.AsyncTask;
+
+/**
+ * Created by Sukhjinder on 2/4/17.
+ */
+
+public class FetchVehicleInfoTask extends AsyncTask<String, Void , String> {
+    @Override
+    protected String doInBackground(String... params) {
+
+
+
+    }
+}
